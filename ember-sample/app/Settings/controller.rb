38aec4cc0643a27e07ca37aa37f3c1a7e6b112ee
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2013-03-15 16:17:52 +0400

    ember-sample: ember-sample is added

---
+++ ember-sample/app/Settings/controller.rb
@@ -0,0 +1,16 @@
+require 'rho'
+require 'rho/rhocontroller'
+require 'rho/rhoerror'
+require 'helpers/browser_helper'
+
+class SettingsController < Rho::RhoController
+  include BrowserHelper
+  
+  def index
+    @msg = @params['msg']
+    render
+  end
+
+
+  
+end
